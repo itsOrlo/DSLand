087be43d562339c69c19107e54831e443761d5db
Orlo Montesdeoca <76452632+itsOrlo@users.noreply.github.com>
2024-07-23 15:36:34 +0000

    Mundo guardado 2024-07-23

--- server.py
+++ server.py
@@ -20,17 +20,7 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-# No toques nada de aquí para abajo, puedes dañarlo
+# Configuración
 import requests,os,base64,glob,time
 if os.path.exists("servidor.py"):
 	os.remove("servidor.py")
